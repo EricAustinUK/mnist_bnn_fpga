8f2ede4ac54129da661d2672f681e6e743e061ad
eric <ericaustin2005@gmail.com>
2026-06-11 18:38:06 +0100

    set SPI DMA access

--- firmware/source/spi.cpp
+++ firmware/source/spi.cpp
@@ -5,11 +5,14 @@ void cnf_spi_pins(){
     if(cfgd) return;
     cfgd = true;
     
-    NRF_SPIM0->PSEL.SCK  = 13; 
-    NRF_SPIM0->PSEL.MISO = 0xFFFFFFFF; 
-    NRF_SPIM0->PSEL.MOSI = 15;
+    NRF_SPIM0->PSEL.SCK  = 13; // telling SPI peripheral to send clock to 13?
+    NRF_SPIM0->PSEL.MISO = 0xFFFFFFFF;  // blanking MISO for now
+    NRF_SPIM0->PSEL.MOSI = 15; // telling SPI peripheral to send over pin 15
 }
 
 void send_arr(uint32_t *arr, uint32_t size){
-    
+
+    NRF_SPIM0->TXD.PTR = uint32_t(arr);
+    NRF_SPIM0->TXD.MAXCNT = size; 
+
 }
