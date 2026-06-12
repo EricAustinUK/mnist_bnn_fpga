25e1be58ca93b5545bf14dcef7c613743025eb9f
eric <ericaustin2005@gmail.com>
2026-06-12 15:09:10 +0100

    implemented timer and quick SPI test, which works on logic analyser

--- firmware/source/main.cpp
+++ firmware/source/main.cpp
@@ -1,9 +1,25 @@
 #include "MicroBit.h"
 
-extern void spi_send_arr(uint8_t * arr, uint32_t size);
+extern void spi_send_arr(volatile uint8_t * arr, uint32_t size);
 
-void busy_sleep(uint32_t type){
+void busy_sleep(uint32_t delay_us){
+    NRF_TIMER1->TASKS_STOP = 1; // stop other timers
+    NRF_TIMER1->BITMODE = 3; // 32 bit
+    NRF_TIMER1->CC[0] = delay_us; // set cmp to time
+    NRF_TIMER1->PRESCALER = 4;
+
+    // clear flags
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
+    NRF_TIMER1->TASKS_CLEAR = 1;
+
+    // wait for delay to be met
+    NRF_TIMER1->TASKS_START = 1;
+    while(NRF_TIMER1->EVENTS_COMPARE[0] == 0);
     
+    // clear flags
+    NRF_TIMER1->TASKS_STOP = 1;
+    NRF_TIMER1->TASKS_CLEAR = 1;
+    NRF_TIMER1->EVENTS_COMPARE[0] = 0;
 }
 
 int main()
@@ -14,6 +30,11 @@ int main()
     NRF_P0->OUTCLR =  (1 << MICROBIT_PIN_COL1);
     NRF_P0->OUTSET =  (1 << MICROBIT_PIN_ROW1);
 
-    
+    volatile uint8_t send_arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    while(true){
+        spi_send_arr(send_arr, 10);
+        busy_sleep(3000000);
+    }
 }
 
